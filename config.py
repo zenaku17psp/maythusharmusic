6c329bbaa458b5e3b5628042fab010caa454b631
zenaku10psp <zenaku10psp@outlook.com>
2025-07-04 09:50:13 +0630

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 
 STICKER_ID = "CAACAgUAAxkBAAEObxloHODEbJMRLG0DgnPYJ7bOUXc5QwACmRkAAg6V6FSBJlu8dUgdCTYE"
 
+COOKIE_URL = getenv("https://batbin.me/leiocome")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
