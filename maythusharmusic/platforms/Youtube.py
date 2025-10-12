1df2afd90d68061aaa7b3aa5d4be32023c648a64
zenaku17psp <zenaku16psp@outlook.com>
2025-10-12 12:13:52 +0630

    Update Youtube.py

--- maythusharmusic/platforms/Youtube.py
+++ maythusharmusic/platforms/Youtube.py
@@ -20,7 +20,7 @@ import time
 
 
 # ✅ Configurable constants
-API_KEY = "Rf1qda5gyCITj6VbrekzRxmR"
+API_KEY = "AIzaSyCXBep3Z6PM1WazfIZAXY9AjkalplwRPj4"
 API_BASE_URL = "http://deadlinetech.site"
 
 MIN_FILE_SIZE = 51200
